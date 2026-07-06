31a7a36514b011cbac883134abbd09ce0581909b
Abdulaziz Fawaz Alotaibi <Az5starrosh@gmail.com>
2026-07-06 11:05:57 +0300

    With basic Command

--- gemeni_test.py
+++ gemeni_test.py
@@ -1,3 +1,11 @@
+# Today we will learn how diffrent prompts will affect of the answers of LLM.
+''' Usually, a good prompts consist of 4 thing :
+   1. ROLE: To specify the role of LLM, for example, "you are a Python expert", "you are a AI teacher".
+   2. Task: what's require? , for example, "Exaplain SQL Injection".
+   3. Constraints: specify the condetion, for example, "Explain it in 150 words", "Use bullet points".
+   4. Output Format (optional): Return the answer as points/table. 
+
+'''
 from google import genai
 from google.genai import types
 
@@ -5,11 +13,15 @@ flag ='y'
 while flag == 'y':
     client = genai.Client()
     temp=input("Detrmine the value: ")
+    answer= input("What do u what to ask: ")
+    print()
     response = client.models.generate_content(
         model="gemini-2.5-flash",
-        contents="what is the capital",
+        contents=answer, # Explain Linux
         config=types.GenerateContentConfig(temperature=temp)
     )
     #print(f"With temperature = {temp}")
     print(response.text)
     flag=input("Do u want to continue? (y/n) :")
+
+# here we notice the answer is more general.
